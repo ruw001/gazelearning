8db89a9a7adc587f5f4204631b16de115bba72a6
ruw001 <ruw001@ucsd.edu>
2020-10-22 00:54:04 -0700

    add calibration page

--- server.js
+++ server.js
@@ -5,11 +5,46 @@ var app = express();
 var http = require('http');
 var server = http.Server(app);
 
-app.use(express.static('client'));
+app.use(express.static('./'));
 
 server.listen(PORT, function () {
     console.log('gaze server running');
 });
+
+var sdk_key = "Mp3WNg7cR5Se5eseb20xX2sRBk4eARKfrsCP";
+var sdk_secret = "Ovbr5wsX7kmgN23dTEn19vSY7fg0Juenl86S";
+var session_ID = "2530883354";
+var session_pwd = "";
+
+function generateInstantToken(sdkKey, sdkSecret, topic, password = "") {
+    let signature = "";
+    // try {
+    const iat = Math.round(new Date().getTime() / 1000);
+    const exp = iat + 60 * 60 * 2;
+
+    // Header
+    const oHeader = { alg: "HS256", typ: "JWT" };
+    // Payload
+    const oPayload = {
+        app_key: sdkKey,
+        iat,
+        exp,
+        tpc: topic,
+        pwd: password,
+    };
+    // Sign JWT
+    const sHeader = JSON.stringify(oHeader);
+    const sPayload = JSON.stringify(oPayload);
+    signature = KJUR.jws.JWS.sign("HS256", sHeader, sPayload, sdkSecret);
+    return signature;
+}
+
+// generateInstantToken(
+//     sdk_key,
+//     sdk_secret,
+//     session_ID,
+//     session_pwd
+// ); // call the generateInstantToken function
 
 // var io = require('socket.io')(server);
 
