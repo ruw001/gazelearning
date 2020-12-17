ca4802d022d443f32b93fcf12b2d73603ca3d39e
ruw001 <ruw001@ucsd.edu>
2020-12-17 01:17:21 -0800

    temp commit

--- js/confusion_detection.js
+++ js/confusion_detection.js
@@ -79,6 +79,57 @@ window.onload = async function () {
             console.log(err)
         });
     }, 5000);
+
+    const videoElement = document.getElementsByClassName('input_video')[0];
+    const canvasElement = document.getElementsByClassName('output_canvas')[0];
+    const canvasCtx = canvasElement.getContext('2d');
+
+    function onResults(results) {
+        canvasCtx.save();
+        canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+        canvasCtx.drawImage(
+            results.image, 0, 0, canvasElement.width, canvasElement.height);
+        if (results.multiFaceLandmarks) {
+            for (const landmarks of results.multiFaceLandmarks) {
+                // drawConnectors(canvasCtx, landmarks, FACEMESH_TESSELATION,
+                //     { color: '#C0C0C070', lineWidth: 1 });
+                drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYE, { color: '#FF3030' });
+                drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYEBROW, { color: '#FF3030' });
+                drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYE, { color: '#30FF30' });
+                drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYEBROW, { color: '#30FF30' });
+                // console.log(FACEMESH_RIGHT_EYE);
+                var srcTri = [landmarks[133], landmarks[362], landmarks[2]]
+                var dstTri = [{x: 150, y: 90}, {x: 170, y: 90}, {x: 120, y: 105}]
+                let { fromTriangles, applyToPoint } = window.TransformationMatrix;
+                var matrix = fromTriangles(srcTri, dstTri);
+                console.log(matrix);
+                // drawConnectors(canvasCtx, landmarks, FACEMESH_FACE_OVAL, { color: '#E0E0E0' });
+                // drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, { color: '#E0E0E0' });
+            }
+        }
+        canvasCtx.restore();
+    }
+
+    const faceMesh = new FaceMesh({
+        locateFile: (file) => {
+            return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+        }
+    });
+    faceMesh.setOptions({
+        maxNumFaces: 1,
+        minDetectionConfidence: 0.75,
+        minTrackingConfidence: 0.75
+    });
+    faceMesh.onResults(onResults);
+
+    const camera = new Camera(videoElement, {
+        onFrame: async () => {
+            await faceMesh.send({ image: videoElement });
+        },
+        width: 320,
+        height: 180
+    });
+    camera.start();
 }
 
 function hideVideoElements() {
@@ -88,6 +139,10 @@ function hideVideoElements() {
     webgazer.showFaceFeedbackBox(false);
     //webgazer.showGazeDot(false);
 };
+
+function afffineTransformation() {
+
+}
 
 async function report(event) {
     if (event.key == 'c') {
