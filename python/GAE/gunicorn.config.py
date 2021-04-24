38ce8964d04013863821a4648bef52a04f0da83a
Voivio <huwindy@hotmail.com>
2021-04-24 13:02:14 +0800

    Smaller number of workers

--- python/GAE/gunicorn.config.py
+++ python/GAE/gunicorn.config.py
@@ -1,7 +1,8 @@
 import multiprocessing
 
 bind = '0.0.0.0:8000'
-workers = multiprocessing.cpu_count() * 3 + 1
+# workers = multiprocessing.cpu_count() * 3 + 1
+workers = 2
 print('{} workers start!'.format(workers))
 # See http://docs.gunicorn.org/en/stable/settings.html#worker-class
 worker_class = 'gevent'
